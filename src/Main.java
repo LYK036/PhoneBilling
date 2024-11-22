b7510dfc76ac92f7ee78bfbac336734b1ae43067
LYK036 <luoyuankang036@gmail.com>
2024-11-22 14:33:24 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,33 @@ public class Main {
     final static double PREMIUM = 20.0;
 
     public static void housekeeping() {
+        // Output "Phone payment calculator"
+        System.out.println("Phone payment calculator");
 
+        // Input customerId, callsMade, and callMinutes
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter customer ID: ");
+        customerID = scanner.nextInt();
+
+        System.out.print("Enter number of calls made: ");
+        callsMade = scanner.nextInt();
+
+        System.out.print("Enter total call minutes: ");
+        callsMinutes = scanner.nextInt();
     }
 
     public static void detailLoop() {
-
+        // To be implemented
     }
 
     public static void finish() {
-
+        // To be implemented
     }
 
     public static void main(String[] args) {
-
+        housekeeping();
+        detailLoop();
+        finish();
     }
 }
